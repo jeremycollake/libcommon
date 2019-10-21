0cefdbe62ca490192a35bf11781bed18069f0e00
jeremy collake <jeremy@bitsum.com>
2019-10-21 15:38:26 -0400

    Simple CPU utilization cache for sharing this data

---
+++ libcommon/processCache.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <map>
+#include <mutex>
+
+class ProcessCache
+{	
+	std::mutex prot;
+	std::map<unsigned int, double> mapCPUUse;
+public:
+	bool add_CPUUse(const unsigned int pid, const double cpuUse)
+	{
+		std::lock_guard<std::mutex> lock(prot);
+		mapCPUUse[pid] = cpuUse;
+		// a safety catch for infinite growth (client didn't call erase)
+		_ASSERT(mapCPUUse.size() < 1000);
+	}
+	void erase(const unsigned int pid)
+	{
+		std::lock_guard<std::mutex> lock(prot);
+		mapCPUUse.erase(pid);
+	}
+	bool get_CPUUse(const unsigned int pid, double& cpuUse)
+	{
+		std::lock_guard<std::mutex> lock(prot);
+		auto i = mapCPUUse.find(pid);		
+		if (i == mapCPUUse.end())
+		{
+			cpuUse = 0;
+			return false;
+		}
+		cpuUse = i->second;
+		return true;
+	}
+};
