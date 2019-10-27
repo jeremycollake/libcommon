1c1567fc4cf30b58d91c6afb1f4a1b145c573847
jeremy collake <jeremy@bitsum.com>
2019-10-27 09:37:54 -0400

    set subclassid

--- libcommon/win32-darkmode/win32-darkmode/ListViewUtil.h
+++ libcommon/win32-darkmode/win32-darkmode/ListViewUtil.h
@@ -16,22 +16,22 @@ void InitListView(const HWND hListView, const int subclassId)
 			{
 			case WM_NOTIFY:			
 			{
-				if (g_darkModeEnabled)
+				if (reinterpret_cast<LPNMHDR>(lParam)->code == NM_CUSTOMDRAW)
 				{
-					if (reinterpret_cast<LPNMHDR>(lParam)->code == NM_CUSTOMDRAW)
+					LPNMCUSTOMDRAW nmcd = reinterpret_cast<LPNMCUSTOMDRAW>(lParam);
+					switch (nmcd->dwDrawStage)
 					{
-						LPNMCUSTOMDRAW nmcd = reinterpret_cast<LPNMCUSTOMDRAW>(lParam);
-						switch (nmcd->dwDrawStage)
-						{
-						case CDDS_PREPAINT:
-							return CDRF_NOTIFYITEMDRAW;
-						case CDDS_ITEMPREPAINT:
+					case CDDS_PREPAINT:
+						return CDRF_NOTIFYITEMDRAW;
+					case CDDS_ITEMPREPAINT:
+					{
+						if (_ShouldAppsUseDarkMode && _ShouldAppsUseDarkMode() && !IsHighContrast())
 						{
 							auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
 							SetTextColor(nmcd->hdc, info->colorHeaderText);
-							return CDRF_DODEFAULT;
 						}
-						}
+						return CDRF_DODEFAULT;
+					}
 					}
 				}
 			}
@@ -41,31 +41,17 @@ void InitListView(const HWND hListView, const int subclassId)
 				HWND hHeader = ListView_GetHeader(hWnd);
 				AllowDarkModeForWindow(hWnd, g_darkModeEnabled);
 				AllowDarkModeForWindow(hHeader, g_darkModeEnabled);
-				if (g_darkModeEnabled)
-				{										
-					HTHEME hTheme = OpenThemeData(nullptr, L"ItemsView");
-					if (hTheme)
-					{
-						COLORREF color;
-						if (SUCCEEDED(GetThemeColor(hTheme, 0, 0, TMT_TEXTCOLOR, &color)))
-						{
-							ListView_SetTextColor(hWnd, color);
-						}
-						if (SUCCEEDED(GetThemeColor(hTheme, 0, 0, TMT_FILLCOLOR, &color)))
-						{
-							ListView_SetTextBkColor(hWnd, color);
-							ListView_SetBkColor(hWnd, color);
-						}
-						CloseThemeData(hTheme);
-					}
-					hTheme = OpenThemeData(hHeader, L"Header");
-					if (hTheme)
-					{
-						auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
-						GetThemeColor(hTheme, HP_HEADERITEM, 0, TMT_TEXTCOLOR, &(info->colorHeaderText));
-						CloseThemeData(hTheme);
-					}										
+				auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
+				info->colorHeaderText = ListView_GetTextColor(hWnd);
+				/*
+				HTHEME hTheme = OpenThemeData(hHeader, L"Header");
+				if (hTheme)
+				{
+					auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
+					GetThemeColor(hTheme, HP_HEADERITEM, 0, TMT_TEXTCOLOR, &(info->colorHeaderText));
+					CloseThemeData(hTheme);
 				}
+				*/
 				SendMessageW(hHeader, WM_THEMECHANGED, wParam, lParam);
 				RedrawWindow(hWnd, nullptr, nullptr, RDW_FRAME | RDW_INVALIDATE);
 			}
@@ -78,7 +64,7 @@ void InitListView(const HWND hListView, const int subclassId)
 			break;
 			}
 			return DefSubclassProc(hWnd, uMsg, wParam, lParam);
-		}, 0, reinterpret_cast<DWORD_PTR>(new SubclassInfo{}));
+		}, subclassId, reinterpret_cast<DWORD_PTR>(new SubclassInfo{}));
 	}
 
 	//ListView_SetExtendedListViewStyle(hListView, LVS_EX_FULLROWSELECT | LVS_EX_DOUBLEBUFFER | LVS_EX_HEADERDRAGDROP);
