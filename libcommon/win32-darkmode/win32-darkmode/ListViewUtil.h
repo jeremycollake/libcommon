8df23c086a066af60cbc73d77f5ad2fc19fbe984
jeremy collake <jeremy@bitsum.com>
2019-11-01 14:44:22 -0400

    further mods to support dynamic toggle of dark mode for an app, regardless of OS setting

--- libcommon/win32-darkmode/win32-darkmode/ListViewUtil.h
+++ libcommon/win32-darkmode/win32-darkmode/ListViewUtil.h
@@ -9,63 +9,54 @@ void InitListView(const HWND hListView, const int subclassId)
 {
 	HWND hHeader = ListView_GetHeader(hListView);
 
-	if (g_darkModeSupported)
-	{
-		SetWindowSubclass(hListView, [](HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR /*uIdSubclass*/, DWORD_PTR dwRefData) -> LRESULT {
-			switch (uMsg)
+	SetWindowSubclass(hListView, [](HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR /*uIdSubclass*/, DWORD_PTR dwRefData) -> LRESULT {
+		switch (uMsg)
+		{
+		case WM_NOTIFY:
+		{
+			if (reinterpret_cast<LPNMHDR>(lParam)->code == NM_CUSTOMDRAW)
 			{
-			case WM_NOTIFY:			
-			{
-				if (reinterpret_cast<LPNMHDR>(lParam)->code == NM_CUSTOMDRAW)
+				LPNMCUSTOMDRAW nmcd = reinterpret_cast<LPNMCUSTOMDRAW>(lParam);
+				switch (nmcd->dwDrawStage)
 				{
-					LPNMCUSTOMDRAW nmcd = reinterpret_cast<LPNMCUSTOMDRAW>(lParam);
-					switch (nmcd->dwDrawStage)
+				case CDDS_PREPAINT:
+					return CDRF_NOTIFYITEMDRAW;
+				case CDDS_ITEMPREPAINT:
+				{
+					if (!g_darkModeSupported || (_ShouldAppsUseDarkMode() && !IsHighContrast()))
 					{
-					case CDDS_PREPAINT:
-						return CDRF_NOTIFYITEMDRAW;
-					case CDDS_ITEMPREPAINT:
-					{
-						if (_ShouldAppsUseDarkMode && _ShouldAppsUseDarkMode() && !IsHighContrast())
-						{
-							auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
-							SetTextColor(nmcd->hdc, info->colorHeaderText);
-						}
-						return CDRF_DODEFAULT;
+						auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
+						SetTextColor(nmcd->hdc, info->colorHeaderText);
 					}
-					}
+					return CDRF_DODEFAULT;
+				}
 				}
 			}
-			break;
-			case WM_THEMECHANGED:
+		}
+		break;
+		case WM_THEMECHANGED:
+		{
+			HWND hHeader = ListView_GetHeader(hWnd);
+			if (g_darkModeSupported)
 			{
-				HWND hHeader = ListView_GetHeader(hWnd);
 				AllowDarkModeForWindow(hWnd, g_darkModeEnabled);
 				AllowDarkModeForWindow(hHeader, g_darkModeEnabled);
-				auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
-				info->colorHeaderText = ListView_GetTextColor(hWnd);
-				/*
-				HTHEME hTheme = OpenThemeData(hHeader, L"Header");
-				if (hTheme)
-				{
-					auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
-					GetThemeColor(hTheme, HP_HEADERITEM, 0, TMT_TEXTCOLOR, &(info->colorHeaderText));
-					CloseThemeData(hTheme);
-				}
-				*/
-				SendMessageW(hHeader, WM_THEMECHANGED, wParam, lParam);
-				RedrawWindow(hWnd, nullptr, nullptr, RDW_FRAME | RDW_INVALIDATE);
 			}
-			break;
-			case WM_DESTROY:
-			{
-				auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
-				delete info;
-			}
-			break;
-			}
-			return DefSubclassProc(hWnd, uMsg, wParam, lParam);
+			auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
+			info->colorHeaderText = ListView_GetTextColor(hWnd);
+			SendMessageW(hHeader, WM_THEMECHANGED, wParam, lParam);
+			RedrawWindow(hWnd, nullptr, nullptr, RDW_FRAME | RDW_INVALIDATE);
+		}
+		break;
+		case WM_DESTROY:
+		{
+			auto info = reinterpret_cast<SubclassInfo*>(dwRefData);
+			delete info;
+		}
+		break;
+		}
+		return DefSubclassProc(hWnd, uMsg, wParam, lParam);
 		}, subclassId, reinterpret_cast<DWORD_PTR>(new SubclassInfo{}));
-	}
 
 	//ListView_SetExtendedListViewStyle(hListView, LVS_EX_FULLROWSELECT | LVS_EX_DOUBLEBUFFER | LVS_EX_HEADERDRAGDROP);
 
