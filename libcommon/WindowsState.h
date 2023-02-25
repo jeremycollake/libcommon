53d242d18c50bfa59db9d0ee865410412833ca64
jeremycollake <jeremy@bitsum.com>
2023-02-25 11:27:36 -0500

    minor

--- libcommon/WindowsState.h
+++ libcommon/WindowsState.h
@@ -7,17 +7,19 @@ namespace WindowsState
 {
 	bool IsFullScreenAppOpen()
 	{
-		QUERY_USER_NOTIFICATION_STATE pquns;
-		if (SHQueryUserNotificationState(&pquns) == S_OK)
+		QUERY_USER_NOTIFICATION_STATE quns;
+		if (SHQueryUserNotificationState(&quns) == S_OK)
 		{
-			switch (pquns)
+			switch (quns)
 			{
 			case QUNS_BUSY:
 			case QUNS_RUNNING_D3D_FULL_SCREEN:
 			case QUNS_PRESENTATION_MODE:
+				LIBCOMMON_DEBUG_PRINT(L"full screen app DETECTED");
 				return true;
 			}
 		}
+		LIBCOMMON_DEBUG_PRINT(L"full screen app NOT detected");
 		return false;
 	}
 };
